9277a78f01bd2e3ee8134ecbf73065dc66b3fcdd
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-06-01 22:40:49 +0200

    Include find_start() and extract() for use instrumentation code

--- src/scaff/analyze.py
+++ src/scaff/analyze.py
@@ -454,3 +454,267 @@ def truncate_min(arr):
     for idx, s in enumerate(arr):
         arr[idx] = s[:target_len]
     return arr
+
+def find_starts(config, data, target_path, index):
+    """
+    Find the starts of interesting activity in the signal.
+
+    The result is a list of indices where interesting activity begins, as well
+    as the trigger signal and its average.
+    """
+    
+    trigger = butter_bandpass_filter(
+        data, config.bandpass_lower, config.bandpass_upper,
+        config.sampling_rate, 6)
+    
+    #TOM ADDITION START
+    #plt.clf()
+    #plt.plot(trigger)
+    #plt.savefig(target_path+"/"+str(index)+"_4-trigger-bandpass.png")
+    #TOM ADDITION END
+        
+    trigger = np.absolute(trigger)
+    # Use an SOS filter because the old one raised exception when using small
+    # lowpass values:
+    lpf = signal.butter(5, config.lowpass_freq, 'low', fs=config.sampling_rate, output='sos')
+    trigger = np.array(signal.sosfilt(lpf, trigger), dtype=trigger.dtype)
+    # trigger = butter_lowpass_filter(trigger, config.lowpass_freq,config.sampling_rate, 6)
+
+    #TOM ADDITION START
+    #plt.clf()
+    #plt.plot(trigger)
+    #plt.savefig(target_path+"/"+str(index)+"_5-trigger-lowpass.png")
+    #TOM ADDITION END
+
+    # transient = 0.0005
+    # start_idx = int(transient * config.sampling_rate)
+    start_idx = 0
+    average = np.average(trigger[start_idx:])
+    maximum = np.max(trigger[start_idx:])
+    minimum = np.min(trigger[start_idx:])
+    middle = (np.max(trigger[start_idx:]) - min(trigger[start_idx:])) / 2
+    if average < 1.1*middle:
+        print("")
+        print("Adjusting average to avg + (max - avg) / 2")
+        average = average + (maximum - average) / 2
+    offset = -int(config.trigger_offset * config.sampling_rate)
+
+    if config.trigger_rising:
+        trigger_fn = lambda x, y: x > y
+    else:
+        trigger_fn = lambda x, y: x < y
+
+    if config.trigger_threshold is not None and config.trigger_threshold > 0:
+        print("Use config trigger treshold instead of average")
+        average = config.trigger_threshold / 100 # NOTE: / 100 because of *100 in plot_results().
+
+    # The cryptic numpy code below is equivalent to looping over the signal and
+    # recording the indices where the trigger crosses the average value in the
+    # direction specified by config.trigger_rising. It is faster than a Python
+    # loop by a factor of ~1000, so we trade readability for speed.
+    trigger_signal = trigger_fn(trigger, average)[start_idx:]
+    starts = np.where((trigger_signal[1:] != trigger_signal[:-1])
+                      * trigger_signal[1:])[0] + start_idx + offset + 1
+    # if trigger_signal[0]:
+    #     starts = np.insert(starts, 0, start_idx + offset)
+
+    #TOM ADDITION START
+    #plt.clf()
+    #plt.plot(trigger_signal)
+    #plt.savefig(target_path+"/"+str(index)+"_6-triggerstart.png")
+    #TOM ADDITION END
+
+
+    # plt.plot(data)
+    # plt.plot(trigger*100)
+    # plt.axhline(y=average*100)
+    # plt.show()
+
+    return starts, trigger, average
+
+# The part that uses a frequency component as trigger was initially
+# inspired by https://github.com/bolek42/rsa-sdr
+# The code below contains a few hacks to deal with all possible errors we
+# encountered with different radios and setups. It is not very clean but it is
+# quite stable.
+def extract(capture_file, config, average_file_name=None, plot=False, target_path=None, savePlot=False, index=0, return_zero=True):
+    """Post-process a GNUradio capture to get a clean and well-aligned trace.
+
+    The configuration is a reproduce.AnalysisConfig tuple. The optional
+    average_file_name specifies the file to write the average to (i.e. the
+    template candidate).
+
+    Return a tuple composed of the extracted amplitude and phase rotation
+    traces.
+
+    """
+    # Load data from custom dtype.
+    data = np.load(capture_file)
+
+    # assert len(data) != 0, "ERROR, empty data just after measuring"
+    if len(data) == 0:
+        print("Warning! empty data, replacing with zeros")
+        template = np.load(config.template_name)
+        return np.zeros(len(template)), np.zeros(len(template))
+
+    #TOM ADDITION START
+    #plt.clf()
+    #plt.plot(data)
+    #plt.savefig(target_path+"/"+str(index)+"_1-data.png")
+    #TOM ADDITION END
+    # plt.plot(data)
+    # plt.show()
+
+    template = np.load(config.template_name) if config.template_name else None
+
+    if template is not None and len(template) != int(
+            config.signal_length * config.sampling_rate):
+        print("WARNING: Template length doesn't match collection parameters. "
+              "Is this the right template?")
+
+    # cut usless transient
+    data = data[int(config.drop_start * config.sampling_rate):]
+
+
+    #TOM ADDITION START
+    #plt.clf()
+    #plt.plot(data)
+    #plt.savefig(target_path+"/"+str(index)+"_2-data-trimmed.png")
+    #TOM ADDITION END
+
+    # assert len(data) != 0, "ERROR, empty data after drop_start"
+    if len(data) == 0:
+       print("Warning! empty data after drop start, replacing with zeros")
+       template = np.load(config.template_name)
+       return np.zeros(len(template)), np.zeros(len(template))
+
+
+    # polar discriminator
+    # fdemod = data[1:] * np.conj(data[:-1])
+    # fdemod = np.angle(fdemod)
+    # plt.plot(fdemod)
+    # plt.show()
+    # return fdemod
+    # data = fdemod
+
+    # AMPlitude
+    data_amp = np.absolute(data)
+    # PHase Rotation
+    data_phr = get_phase_rot(data)
+    # I
+    data_i = np.real(data)
+    # Q
+    data_q = np.imag(data)
+
+    # Compute augmented I/Q.
+    data_augmented = data_amp * np.exp(1j * data_phr)
+    # I from augmented IQ.
+    data_i_augmented = np.real(data_augmented)
+    # Q from augmented IQ.
+    data_q_augmented = np.imag(data_augmented)
+
+    #TOM ADDITION START
+    #plt.clf()
+    #plt.plot(data)
+    #plt.savefig(target_path+"/"+str(index)+"_3-data-absolute.png")
+    #TOM ADDITION END
+    #
+    # extract/aling trace with trigger frequency + autocorrelation
+    #
+    # NOTE: find_starts() will work with the amplitude, but we will use the
+    # starts indexes against the raw I/Q.
+    trace_starts, trigger, trigger_avg = find_starts(config, data_amp, target_path, index)
+
+    # extract at trigger + autocorrelate with the first to align
+    traces_amp = []
+    traces_phr = []
+    traces_i = []
+    traces_q = []
+    traces_i_augmented = []
+    traces_q_augmented = []
+    trace_length = int(config.signal_length * config.sampling_rate)
+    print("INFO: Number of starts: {}".format(len(trace_starts)))
+    for start_idx, start in enumerate(trace_starts):
+        if len(traces_amp) >= min(config.num_traces_per_point, config.num_traces_per_point_keep):
+            break
+
+        stop = start + trace_length
+
+        if stop > len(data_amp):
+            break
+
+        trace_amp = data_amp[start:stop]
+        if template is None or len(template) == 0:
+            template = trace_amp
+            continue
+
+        trace_lpf = butter_lowpass_filter(trace_amp, config.sampling_rate / 4,
+                config.sampling_rate)
+        template_lpf = butter_lowpass_filter(template, config.sampling_rate / 4,
+                config.sampling_rate)
+        correlation = signal.correlate(trace_lpf**2, template_lpf**2)
+        print("INFO: corr={}".format(max(correlation)))
+        if max(correlation) <= config.min_correlation:
+            print("WARN: Skip trace start: corr={} <= corr_min={}".format(max(correlation), config.min_correlation))
+            continue
+
+        shift = np.argmax(correlation) - (len(template)-1)
+        traces_amp.append(data_amp[start+shift:stop+shift])
+        traces_phr.append(data_phr[start+shift:stop+shift])
+        traces_i.append(data_i[start+shift:stop+shift])
+        traces_q.append(data_q[start+shift:stop+shift])
+        traces_i_augmented.append(data_i_augmented[start+shift:stop+shift])
+        traces_q_augmented.append(data_q_augmented[start+shift:stop+shift])
+
+    avg_amp = np.average(traces_amp, axis=0)
+    avg_phr = np.average(traces_phr, axis=0)
+    avg_i = np.average(traces_i, axis=0)
+    avg_q = np.average(traces_q, axis=0)
+    avg_i_augmented = np.average(traces_i_augmented, axis=0)
+    avg_q_augmented = np.average(traces_q_augmented, axis=0)
+
+    if return_zero is True and (
+            (np.shape(avg_amp) == () or np.shape(avg_phr) == ()
+             or np.shape(avg_i) == () or np.shape(avg_q) == ()
+             or np.shape(avg_i_augmented) == () or np.shape(avg_q_augmented) == ())
+    ):
+        return np.zeros(len(template)), np.zeros(len(template)), np.zeros(len(template)), np.zeros(len(template)), np.zeros(len(template)), np.zeros(len(template))
+    elif return_zero is False and (
+            (np.shape(avg_amp) == () or np.shape(avg_phr) == ()
+             or np.shape(avg_i) == () or np.shape(avg_q) == ()
+             or np.shape(avg_i_augmented) == () or np.shape(avg_q_augmented) == ())
+    ):
+        if plot or savePlot:
+            plot_results(config, data_amp, trigger, trigger_avg, trace_starts, traces_amp, target_path, plot, savePlot, "amp")
+            plot_results(config, data_phr, trigger, trigger_avg, trace_starts, traces_phr, target_path, plot, savePlot, "phr")
+        raise Exception("Trigger or correlation configuration excluded all starts!")
+
+    if average_file_name:
+        np.save(average_file_name, avg_amp)
+
+    if plot or savePlot:
+        plot_results(config, data_amp, trigger, trigger_avg, trace_starts, traces_amp, target_path, plot, savePlot, "amp")
+        plot_results(config, data_phr, trigger, trigger_avg, trace_starts, traces_phr, target_path, plot, savePlot, "phr")
+
+    std = np.std(traces_amp,axis=0)
+
+    print("Extracted ")
+    print("Number = ",len(traces_amp))
+    print("avg[Max(std)] = %.2E"%avg_amp[std.argmax()])
+    print("Max(u) = Max(std) = %.2E"%(max(std)))
+    print("Max(u_rel) = %.2E"%(100*max(std)/avg_amp[std.argmax()]),"%")
+
+    # plt.plot(avg, 'r')
+    # plt.plot(template, 'b')
+    # plt.show()
+
+    if config.keep_all:
+        return traces_amp
+    else:
+        return avg_amp, avg_phr, avg_i, avg_q, avg_i_augmented, avg_q_augmented
+
+    # except Exception as inst:
+    #     print(inst)
+    #     print("Error, returning zeros")
+    #     template = np.load(config.template_name)
+    #     return np.zeros(len(template)), np.zeros(len(template)), np.zeros(len(template)), np.zeros(len(template)), np.zeros(len(template)), np.zeros(len(template))

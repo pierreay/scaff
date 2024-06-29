816a9d725bfccfec914c5086faa4e93b0275080e
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-06-29 11:48:45 +0200

    Init the triggers.py module

---
+++ src/scaff/triggers.py
@@ -0,0 +1,17 @@
+"""Offline triggering module.
+
+The main purpose of triggers.py is to provides way to extract and process
+signals based on signal processing and pattern matching.
+
+"""
+
+# * Importation
+
+# Standard import.
+
+# External import.
+
+# Internal import.
+from scaff import logger as l
+
+# * TODO
